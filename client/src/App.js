9e8c73194b8fbada683e069c15feb7feb4f177a0
Mayur Koli <kolimayurs@gmail.com>
2018-05-11 16:32:37 +0530

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,127 @@
 import React from 'react';
-
+import './style.css'
 
 class App extends React.Component {
   constructor(props) {
         super(props);
         this.state = {
+            loading: false,
             products: [],
-            product:{
-              name: '',
-              price: 0
-            }
+            name: '',
+            price:0,
+            edit: false,
+            oldname: ''
         }
+         this.getProducts = this.getProducts.bind(this);
         this.onChangeName = this.onChangeName.bind(this);
         this.onChangePrice = this.onChangePrice.bind(this);
         this.addProducts = this.addProducts.bind(this);
-
+        this.delProducts = this.delProducts.bind(this);
+        this.editProducts = this.editProducts.bind(this);
+        this.upadetProducts = this.upadetProducts.bind(this);
+        this.cancelUpdate = this.cancelUpdate.bind(this);
     }
+
+
     componentDidMount(){
       this.getProducts();
     }
     getProducts(){
+      this.setState({loading: true})
       fetch('/products')
       .then(res => res.json())
-      .then(res => this.setState({products: res.data}));
+      .then(res => this.setState({products: res.data,loading: false, name: '', price:0}));
     }
     onChangeName(e){
       this.setState({
-        ...this.state.product,
-        product:{
           name: e.currentTarget.value
-        }
       });
     }
      onChangePrice(e){
       this.setState({
-        ...this.state.product,
-        product:{
-          price: e.currentTarget.value
-        }
+          price: Number(e.currentTarget.value)
       });
     }
 
 
     addProducts(){
-      console.log(this.state.product.name + ' | ' + this.state.product.price);
-      fetch('/products/add?name='+this.state.product.name+'&price='+this.state.product.price)
+      this.setState({loading: true})
+      fetch('/products/add?name='+this.state.name+'&price='+Number(this.state.price))
       .then(this.getProducts)
       .catch(err => console.error(err));
     }
+    delProducts(name){
+      this.setState({loading: true})
+      fetch('/products/delete?name='+name)
+      .then(this.getProducts)
+      .catch(err => console.error(err));
+    }
+    editProducts(name, price){
+      this.setState({edit: true, name: name, price: Number(price), oldname: name});
+    }
+
+    upadetProducts(name){
+       this.setState({loading: true})
+      fetch('/products/update?oldname='+name+'&name='+this.state.name+'&price='+Number(this.state.price))
+      .then(this.getProducts)
+      .catch(err => console.error(err));
+      this.setState({edit: false, name: '', price:0});
+    }
+    cancelUpdate(){
+      this.setState({edit: false, name: '', price: 0});
+    }
   render() {
+    if(this.state.loading){
+      return(<h1>Loading...</h1>)
+    }
+    let Button = <button onClick={this.addProducts.bind(this)}>ADD</button>;
+    if(this.state.edit){
+      Button = (<div className="inline-block">
+        <button onClick={this.upadetProducts.bind(this, this.state.oldname)}>Update</button>
+        <button onClick={this.cancelUpdate.bind(this)}>Cancel</button>
+        </div>)
+    }
+    else{
+      Button = (<div className="inline-block">
+        <button onClick={this.addProducts.bind(this)}>ADD</button>
+        </div>)
+    }
+    const products_data = this.state.products.map((product,i) => {
+     /* <ul>
+          {this.state.products.map((product,i) =>
+            <li key={'product_'+i}>{product.name} | {product.price} <button  onClick={this.editProducts.bind(this, product.name, product.price)}>Edit</button>  <button  onClick={this.delProducts.bind(this, product.name)}>Delete</button></li>
+            )}
+        </ul>*/
+      return(<tr key={'product_'+i}>
+              <td>{product._id}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td><button  onClick={this.editProducts.bind(this, product.name, product.price)}>Edit</button></td>
+              <td><button  onClick={this.delProducts.bind(this, product.name)}>Delete</button></td>
+        </tr>)
+    })
     return (
       <div className="App">
         <h1>Products</h1>
-        <ul>
-          {this.state.products.map(product =>
-            <li key={product.product_id}>{product.name} | {product.price}</li>
-            )}
-        </ul>
-        <div>
-          <input value={this.state.product.name} onChange= {this.onChangeName.bind(this)} />
-          <input value={this.state.product.price} onChange= {this.onChangePrice.bind(this)}  />
-          <button onClick={this.addProducts.bind(this)}>Add</button>
-        </div>
+          <div className="input-section">
+            <input value={this.state.name} Placeholder="Enter Name Here..." onChange= {this.onChangeName.bind(this)} />
+            <input value={this.state.price} Placeholder="Enter Price Here..." onChange= {this.onChangePrice.bind(this)}  />
+          {Button}
+          </div>
+          <table cellPadding="0" cellSpacing="0" border="0">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Action_Edit</th>
+                <th>Action_Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products_data}
+            </tbody>
+          </table>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,58 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql');
 
 const app = express();
+var MongoClient = require('mongodb').MongoClient;
 
-const SELECT_ALL_PRODUCT_QUERY = "SELECT * FROM  `products`";
 
-/*const connection = mysql.createConnection({
-	host: 'mysql.hostinger.in',
-	user: 'u353447878_pro',
-	password: '29031991',
-	database: 'u353447878_pro',
-});*/
-	var self = this;
-    var pool = mysql.createPool({
-        connectionLimit : 100,
-        waitForConnections : true,
-        queueLimit :0,
-        /*host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'test',*/
-		host: 'sql12.freesqldatabase.com',
-	user: 'sql12231552',
-	password: 'H8NCm6n34G',
-	database: 'sql12231552',
-        debug    :  true,
-        wait_timeout : 28800,
-        connect_timeout :10
-    });
+const url  ='mongodb+srv://kolimayurs:29031991@cluster0-onizc.mongodb.net';
 
-/*  var self = this;
-    var pool = mysql.createPool({
-        connectionLimit : 100,
-        waitForConnections : true,
-        queueLimit :0,
-        hostname: 'mysql.hostinger.in',
-		user: 'u353447878_macwy',
-		password: '29031991',
-		database: 'u353447878_macwy',
-        debug    :  true,
-        wait_timeout : 28800,
-        connect_timeout :10
-    });*/
 
-/*connection.connect(err => {
-	if(err){
-		return err;
-		console.log(err);
-	}
-	console.log('connected');
-});
-*/
-//console.log(connection);
 
 app.use(cors());
 
@@ -60,143 +15,81 @@ app.get('/', (req,res) =>{
 	res.send('go to /products to see products');
 });
 
-app.get('/products/add', (req,res) =>{
-	const INSERT_INTO_PRODUCT = 'INSERT INTO products (name, price) VALUES ("'+req.query.name+'", '+req.query.price+')';
-	pool.query(INSERT_INTO_PRODUCT, (err, results)=>{
-		if(err){
-		res.send(err);
-	}
-	else{
-		res.send("Product Succesfully added");
-	}
-	});
-	
-});
-app.get('/products/update', (req, res) =>{
-	const UPDATE_PRODUCT_QUERY = 'UPDATE products SET price = '+req.query.price+' WHERE product_id = '+req.query.product_id+'';
-	pool.query(UPDATE_PRODUCT_QUERY, (err, result) =>{
-		if(err){
-			res.send(err);
-		}
-		else{
-			res.send('Product price updated');
-		}
-	});
-});
-
-app.get('/products/sort/price/high', (req, res) =>{
-	const SORT_PRODUCT_QUERY = 'SELECT * FROM  `products` ORDER BY price DESC';
-	pool.query(SORT_PRODUCT_QUERY, (err, results) =>{
-		if(err){
-			//console.log('Ye hai Error ');
-			return res.send(err);
-		}
-		else{
-			//console.log('Succesful');
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/products/filter/price', (req, res) =>{
-	if (req.query.low == null || req.query.high == null){
-		SORT_PRODUCT_QUERY = 'SELECT * FROM  `products` ORDER BY price ASC';
-}
-	else{
-		SORT_PRODUCT_QUERY = 'SELECT * FROM  `products` WHERE price BETWEEN '+req.query.low+' AND '+req.query.high+'';
-	}
-	
-	pool.query(SORT_PRODUCT_QUERY, (err, results) =>{
-		if(err){
-			//console.log('Ye hai Error ');
-			return res.send(err);
-		}
-		else{
-			//console.log('Succesful');
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/products/sort/name/desc', (req, res) =>{
-	const SORT_PRODUCT_QUERY = 'SELECT * FROM  `products` ORDER BY name DESC';
-	pool.query(SORT_PRODUCT_QUERY, (err, results) =>{
-		if(err){
-			//console.log('Ye hai Error ');
-			return res.send(err);
-		}
-		else{
-			//console.log('Succesful');
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/products/sort/name/asc', (req, res) =>{
-	const SORT_PRODUCT_QUERY = 'SELECT * FROM  `products` ORDER BY name ASC';
-	pool.query(SORT_PRODUCT_QUERY, (err, results) =>{
-		if(err){
-			//console.log('Ye hai Error ');
-			return res.send(err);
-		}
-		else{
-			//console.log('Succesful');
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/products/sort/price/low', (req, res) =>{
-	const SORT_PRODUCT_QUERY = 'SELECT * FROM  `products` ORDER BY price ASC';
-	pool.query(SORT_PRODUCT_QUERY, (err, results) =>{
-		if(err){
-			//console.log('Ye hai Error ');
-			return res.send(err);
-		}
-		else{
-			//console.log('Succesful');
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-
-app.get('/products/delete', (req, res) => {
-	const DELETE_PRODUCT_QUERY = 'DELETE FROM products WHERE product_id = '+req.query.product_id+'';
-	pool.query(DELETE_PRODUCT_QUERY, (err, results) =>{
-		if(err){
-			res.send(err);
-		}
-		else{
-			res.send('Product deleted Succesfully');
-		}
-	});
-});
 app.get('/products', (req,res) =>{
-	pool.query(SELECT_ALL_PRODUCT_QUERY, (err, results) =>{
-		if(err){
-			//console.log('Ye hai Error ');
+	MongoClient.connect(url, function(err, db) {
+  if(err){
+			return res.send(err);
+		}
+  var dbo = db.db("myDB");
+  dbo.collection("Products").find({}).toArray(function(err, result)  {
+    if(err){
 			return res.send(err);
 		}
 		else{
-			//console.log('Succesful');
 			return res.json({
-				data: results
+				data: result
 			})
 		}
-	});
-	//res.send('products');
+    db.close();
+  });
+}); 
 });
+
+app.get('/products/add', (req,res) =>{
+	MongoClient.connect(url, function(err, db) {
+  if(err){
+			return res.send(err);
+		}
+  var dbo = db.db("myDB");
+  var myobj = { name: req.query.name, price: Number(req.query.price) };
+  dbo.collection("Products").insertOne(myobj, function(err, result) {
+    if(err){
+			return res.send(err);
+		}
+    console.log("1 document inserted");
+    res.send('1 document inserted');
+    db.close();
+  });
+});
+});
+
+app.get('/products/delete', (req,res) =>{
+	MongoClient.connect(url, function(err, db) {
+  if(err){
+			return res.send(err);
+		}
+  var dbo = db.db("myDB");
+  var myobj = { name: req.query.name};
+  dbo.collection("Products").deleteOne(myobj, function(err, result) {
+    if(err){
+			return res.send(err);
+		}
+    console.log("1 document deleted");
+    res.send('1 document deleted');
+    db.close();
+  });
+});
+});
+
+app.get('/products/update', (req,res) =>{
+  MongoClient.connect(url, function(err, db) {
+  if(err){
+      return res.send(err);
+    }
+  var dbo = db.db("myDB");
+   var myquery = { name: req.query.oldname};
+  var newvalues = { $set: {name: req.query.name, price: Number(req.query.price) } };
+  dbo.collection("Products").updateOne(myquery, newvalues, function(err, result) {
+    if(err){
+      return res.send(err);
+    }
+    console.log("1 document update");
+    res.send('1 document update');
+    db.close();
+  });
+});
+});
+
 var port=Number(process.env.PORT || 4000);
 app.listen(port, () => {
 	console.log('Product server listing from port ' + port);
